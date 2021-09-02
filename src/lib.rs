977612968b0f7b48f9b818f05ea958484b705332
Sankar boro <sankar.boro@yahoo.com>
2021-09-02 20:25:46 +0530

    mini improvements

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use ink_lang as ink;
 
 #[ink::contract]
 mod reserve_bank {
+    use core::option::Option::{ self, None };
     use ink_prelude::vec::Vec;
     use ink_storage::{
         collections::{
@@ -21,8 +22,8 @@ mod reserve_bank {
         balances: StorageHashMap<AccountId, Balance>,
         /// Mapping of the token amount which an account is allowed to withdraw
         /// from another account.
-        borrowers: StorageHashMap<AccountId, (AccountId, Balance)>,
-        _b: StorageHashMap<AccountId, Vec<(AccountId, Balance)>>
+        _a: StorageHashMap<AccountId, Option<Vec<(AccountId, Balance)>>>,
+        _b: StorageHashMap<AccountId, Option<Vec<(AccountId, Balance)>>>
     }
 
     /// Event emitted when a token transfer occurs.
@@ -79,7 +80,7 @@ mod reserve_bank {
             let instance = Self {
                 reserved_balance: Lazy::new(reserved_balance),
                 balances,
-                borrowers: StorageHashMap::new(),
+                _a: StorageHashMap::new(),
                 _b: StorageHashMap::new(),
             };
             Self::env().emit_event(Transfer {
@@ -108,19 +109,42 @@ mod reserve_bank {
         }
 
         #[ink(message)]
-        pub fn my_borrowed_balance(&self) -> (AccountId, Balance) {
-            let _self = self.env().caller();
-            self.borrowers.get(&_self).copied().unwrap()
-        }
-
-        #[ink(message)]
-        pub fn borrowed_balance_of(&self, owner: AccountId) -> (AccountId, Balance) {
-            self.borrowers.get(&owner).copied().unwrap()
+        pub fn borrowed_balance_of(&self, user_id: AccountId) -> Option<Balance> {
+            let a= self._b.get(&user_id).unwrap_or(&None).clone();
+            match a {
+                Some(_a) => {
+                    let mut t = None;
+                    _a.into_iter().for_each(|(a, b)| { 
+                        if a == user_id {
+                            t = Some(b);
+                        }
+                    });
+                    t
+                }
+                None => None,
+            }
         }
 
         #[ink(message)]
         pub fn borrow(&mut self, from: AccountId, to: AccountId, value: Balance) -> Result<()> {
-            self.borrow_from_to(from, to, value)
+            self.decrease_balance(from, value);
+            self.increase_borrow_balance(from, to, value);
+            Ok(())
+        }
+
+        fn deduce(&mut self, user_id: AccountId, ori: Balance, value: Balance) {
+            self.balances.insert(user_id, ori - value);
+        }
+
+        fn decrease_balance(&mut self, from: AccountId, value: Balance) {
+            let b: Balance = self.balance_of(from);
+            self.deduce(from, b, value);
+        }
+
+
+        fn increase_borrow_balance(&mut self, from: AccountId, to: AccountId, value: Balance) {
+            // increase borrow balance from to
+            // cause we have send it to to.
         }
         
         #[ink(message)]
@@ -130,12 +154,12 @@ mod reserve_bank {
 
         #[ink(message)]
         pub fn send_documents(&mut self, to: AccountId, value: Balance, file: Vec<u8>) -> Result<()> {
-            let _self = self.env().caller();
-            let a = self.borrowers.get(&_self).copied().unwrap();
-            let b: AccountId = a.0;
-            let m = self.balances.get(&b).copied().unwrap();
-            self.balances.insert(b, m + value);
-            self.borrowers.insert(_self, (b, 0));
+            // let _self = self.env().caller();
+            // let a = self.borrowers.get(&_self).copied().unwrap();
+            // let b: AccountId = a.0;
+            // let m = self.balances.get(&b).copied().unwrap();
+            // self.balances.insert(b, m + value);
+            // self.borrowers.insert(_self, (b, 0));
             Ok(())
         }
 
@@ -166,18 +190,18 @@ mod reserve_bank {
             to: AccountId,
             value: Balance,
         ) -> Result<()> {
-            let from_balance = self.balance_of(from);
-            if from_balance < value {
-                return Err(Error::InsufficientBalance)
-            }
-            self.balances.insert(from, from_balance - value);
+            // let from_balance = self.balance_of(from);
+            // if from_balance < value {
+            //     return Err(Error::InsufficientBalance)
+            // }
+            // self.balances.insert(from, from_balance - value);
             
-            self.borrowers.insert(to, (from, value));
-            self.env().emit_event(Borrow {
-                from: Some(from),
-                to: Some(to),
-                value,
-            });
+            // self.borrowers.insert(to, (from, value));
+            // self.env().emit_event(Borrow {
+            //     from: Some(from),
+            //     to: Some(to),
+            //     value,
+            // });
             Ok(())
         }
 
